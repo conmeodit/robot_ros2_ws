08cbc381a71c61aee9327d15ab0a1fe2c0449361
conmeodit <hailinh13092006@gmail.com>
2026-03-19 23:39:47 +0700

    sua vi chua co map

--- src/robot_bringup/launch/pi_localization.launch.py
+++ src/robot_bringup/launch/pi_localization.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -9,22 +9,25 @@ def generate_launch_description():
 	slam_params = PathJoinSubstitution(
 		[FindPackageShare('robot_bringup'), 'config', 'slam.yaml']
 	)
+	slam_launch = PathJoinSubstitution(
+		[FindPackageShare('slam_toolbox'), 'launch', 'online_async_launch.py']
+	)
 
 	use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='false')
+	autostart_arg = DeclareLaunchArgument('autostart', default_value='true')
 
-	slam_toolbox = Node(
-		package='slam_toolbox',
-		executable='async_slam_toolbox_node',
-		name='slam_toolbox',
-		output='screen',
-		parameters=[
-			slam_params,
-			{'use_sim_time': LaunchConfiguration('use_sim_time')},
-		],
+	slam_toolbox = IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(slam_launch),
+		launch_arguments={
+			'slam_params_file': slam_params,
+			'use_sim_time': LaunchConfiguration('use_sim_time'),
+			'autostart': LaunchConfiguration('autostart'),
+		}.items(),
 	)
 
 	return LaunchDescription([
 		use_sim_time_arg,
+		autostart_arg,
 		slam_toolbox,
 	])
 
