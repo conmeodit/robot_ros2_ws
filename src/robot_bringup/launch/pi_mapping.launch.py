3cdfe0af565325928bee9ddc1ada83625786388c
conmeodit <hailinh13092006@gmail.com>
2026-03-22 12:03:42 +0700

    file anh hung

--- src/robot_bringup/launch/pi_mapping.launch.py
+++ src/robot_bringup/launch/pi_mapping.launch.py
@@ -1,1 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
 
+def generate_launch_description():
+    bringup_dir = get_package_share_directory('robot_bringup')
+    desc_dir = get_package_share_directory('robot_description')
+    
+    # 1. Xử lý file URDF
+    xacro_file = os.path.join(desc_dir, 'urdf', 'robot.urdf.xacro')
+    doc = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': doc.toxml()}
+
+    # Node State Publisher (Đọc URDF)
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    # 2. Khởi chạy Lidar A1M8
+    rplidar_node = Node(
+        package='rplidar_ros',
+        executable='rplidar_node',
+        name='rplidar_node',
+        parameters=[{'serial_port': '/dev/ttyUSB0', 'frame_id': 'laser', 'angle_compensate': True}],
+        output='screen'
+    )
+
+    # 3. Tạo khung liên kết odom -> base_link (Mô phỏng do chưa có node Odom thực tế)
+    static_tf_odom = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link']
+    )
+
+    # 4. SLAM Toolbox Node
+    slam_params_file = os.path.join(bringup_dir, 'config', 'slam.yaml')
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py'
+        )]),
+        launch_arguments={'slam_params_file': slam_params_file}.items()
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        static_tf_odom,
+        rplidar_node,
+        slam_toolbox
+    ])
