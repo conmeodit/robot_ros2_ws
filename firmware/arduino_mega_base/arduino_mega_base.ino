16bbd6b81c7305a99b29477f878fdff0c18fea6e
conmeodit <hailinh13092006@gmail.com>
2026-04-24 16:33:10 +0700

    check i2c

--- firmware/arduino_mega_base/arduino_mega_base.ino
+++ firmware/arduino_mega_base/arduino_mega_base.ino
@@ -2,17 +2,25 @@
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
-  while (!Serial); // Đợi Serial Monitor mở
   
-  Serial.println("\n--- Bắt đầu quét I2C ---");
+  // Khởi tạo Serial máy tính (dự phòng)
+  Serial.begin(115200); 
+  
+  // Khởi tạo giao tiếp với HC-05 qua cổng Serial2 của Mega
+  Serial2.begin(9600);  
+  
+  delay(2000); // Chờ HC-05 khởi động
+  
+  Serial.println("\n--- Bat dau quet I2C ---");
+  Serial2.println("\n--- Bat dau quet I2C ---");
 }
 
 void loop() {
   byte error, address;
   int nDevices;
 
-  Serial.println("Đang quét...");
+  Serial.println("Dang quet...");
+  Serial2.println("Dang quet...");
 
   nDevices = 0;
   // Quét các địa chỉ từ 1 đến 127
@@ -21,25 +29,41 @@ void loop() {
     error = Wire.endTransmission();
 
     if (error == 0) {
-      Serial.print("Tìm thấy thiết bị I2C tại địa chỉ 0x");
-      if (address < 16) 
+      Serial.print("Tim thay I2C tai dia chi 0x");
+      Serial2.print("Tim thay I2C tai dia chi 0x");
+      
+      if (address < 16) {
         Serial.print("0");
+        Serial2.print("0");
+      }
+      
       Serial.println(address, HEX);
+      Serial2.println(address, HEX);
+      
       nDevices++;
     }
     else if (error == 4) {
-      Serial.print("Lỗi không xác định tại địa chỉ 0x");
-      if (address < 16) 
+      Serial.print("Loi khong xac dinh tai 0x");
+      Serial2.print("Loi khong xac dinh tai 0x");
+      
+      if (address < 16) {
         Serial.print("0");
+        Serial2.print("0");
+      }
+      
       Serial.println(address, HEX);
+      Serial2.println(address, HEX);
     }    
   }
   
   if (nDevices == 0) {
-    Serial.println("Không tìm thấy thiết bị I2C nào.\n");
+    Serial.println("Khong tim thay module I2C nao.\n");
+    Serial2.println("Khong tim thay module I2C nao.\n");
   } else {
-    Serial.println("Quét xong.\n");
+    Serial.println("Quet xong.\n");
+    Serial2.println("Quet xong.\n");
   }
 
-  delay(5000); // Chờ 5 giây rồi quét lại
+  // Chờ 5 giây rồi tiến hành quét lại
+  delay(5000); 
 }
